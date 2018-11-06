3722e46ea53804e1ca2ec3aca7ebf930965a41b9
Kaif Ahmed <hello@kaif.me>
2018-11-06 13:44:01 +0000

    created seeds file and added in raise event in tasks#index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
-def index
-end
+  def index
+    @tasks = Task.all
+    raise
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create({title: "get lunch", details: "you need to eat unfortunately"})
+Task.create({title: "finish challenges", details: "you need to finish the challenges"})
+Task.create({title: "do flashcards", details: "more things"})
